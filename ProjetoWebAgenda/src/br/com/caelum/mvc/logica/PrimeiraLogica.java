267412c7e8a4cebf7e93292099d2839e85a3ac55
jhondonavan <jhondonavan@gmail.com>
2016-03-11 18:39:35 -0300

    tentando fazer MVC funcionar perfeitamente

--- ProjetoWebAgenda/src/br/com/caelum/mvc/logica/PrimeiraLogica.java
+++ ProjetoWebAgenda/src/br/com/caelum/mvc/logica/PrimeiraLogica.java
@@ -4,11 +4,12 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 public class PrimeiraLogica implements Logica{
+	
 	public String executa(HttpServletRequest req,
-			HttpServletResponse res) throws Exception{
-		
+		HttpServletResponse res) throws Exception {
 		System.out.println("Executando a logica ...");
 		System.out.println("Retornando o nome da página JSP ...");
 		return "primeira-logica.jsp";
-	};
-}
+		}
+	}
+
